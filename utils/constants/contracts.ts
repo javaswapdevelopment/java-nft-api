5cf8ed8ee7e477ba70183416b06be05c594f2708
javaswap <javaswap.development@gmail.com>
2022-03-01 15:44:12 -0500

    Change the name for the contract

--- utils/constants/contracts.ts
+++ utils/constants/contracts.ts
@@ -2,7 +2,7 @@ export default [
   {
     address: "0x7b30339338Ea8d7e1eC85CD383807CD068dEfaF3",
     owner: "0x159008B72da36E2c51651896B881644e62C1042E|",
-    name: "Presale JAVASWAP",
+    name: "JAVASWAP NFT",
     description:
       "To immortalise our launch with our partner Polygon, we have rewarded our first 1000 earliest supporters and believers with the rarest of NFTs. The creation of a better future lies in your hands! Long live JavaSwap and its supporters!",
     avatar: "https://nft.javaswap.io/0x7b30339338Ea8d7e1eC85CD383807CD068dEfaF3/avatar.png",
